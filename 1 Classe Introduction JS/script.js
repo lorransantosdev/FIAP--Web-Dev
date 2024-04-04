e2bc372fc20db3438bed4b8372540eaef05a433e
lorransantosdev <lorransantossarmentol@gmail.com>
2024-04-03 22:48:14 -0300

    Iniciando JS

---
+++ 1 Classe Introduction JS/script.js
@@ -0,0 +1,33 @@
+/* // ; is opticional   - ALT + SHIFT + A (comenta em mais linhas)
+
+alert('Hello World!');
+// input
+prompt("Digite seu nome");
+// pergunta
+confirm("Você deseja sair?");
+
+// console displays errors (debug)
+
+console.log("Lorran")
+
+// para guardar as coisas usa as variaveis
+// var name = 'value, o que ela recebe';  --> estrutura de variavel inicial
+
+var nome = "Luana";
+var idade = 18;
+console.log(nome+" "+idade);
+
+//enter
+var nome2 = prompt("Enter your name");
+//Exit
+console.log(`Olá ${nome2}, seja bem-vindo!`); */
+
+// First challenge
+
+//input
+var nome = prompt("ENTER YOUR NAME, PLEASE")
+var idade = prompt("ENTER YOUR AGE, PLEASE")
+var profissao = prompt("ENTER YOUR PROFILE, PLEASE")
+
+//exit
+console.log(`Hello ${nome}, you are ${idade} years old and working with ${profissao}.`)
